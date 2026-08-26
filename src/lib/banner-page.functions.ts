2e14bb9e1a30d3f2b9fc9463ea8912329c620c2c
LuceatLuxVestra42 <sapereaude1618@gmail.com>
2026-08-26 22:26:15 +0900

    Trigger banner prerender validation

--- src/lib/banner-page.functions.ts
+++ src/lib/banner-page.functions.ts
@@ -1,5 +1,6 @@
 import { readBannerPageData } from "./banner-page.static";
 
+// Static fixture used to verify /banners prerendering for GitHub Pages.
 export function getBannerPageData() {
   return readBannerPageData();
 }
