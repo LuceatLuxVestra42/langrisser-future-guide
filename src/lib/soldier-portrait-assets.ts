9f4ff110989a1414d2b0aa9b8539326b749c7964
LuceatLuxVestra42 <sapereaude1618@gmail.com>
2026-08-27 21:05:16 +0900

    Connect normal Soldier portrait closeout manifest

--- src/lib/soldier-portrait-assets.ts
+++ src/lib/soldier-portrait-assets.ts
@@ -1,4 +1,4 @@
-import soldierPortraitManifestJson from "../../data/generated/soldier-portrait-manifest.v5.json";
+import soldierPortraitManifestJson from "../../data/generated/soldier-portrait-manifest.v6.json";
 
 type SoldierPortraitManifestRecord = {
   soldierId: number;
