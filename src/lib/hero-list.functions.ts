be9ea7ddc25b511a5717ec5040c739df4fd4a5eb
LuceatLuxVestra42 <sapereaude1618@gmail.com>
2026-08-27 18:16:29 +0900

    Expose Hero Stage 3 list data

--- src/lib/hero-list.functions.ts
+++ src/lib/hero-list.functions.ts
@@ -1,7 +1,11 @@
 import { createServerFn } from "@tanstack/react-start";
 
-import { readHeroListStage2Data } from "./hero-list.server";
+import { readHeroListStage2Data, readHeroListStage3Data } from "./hero-list.server";
 
 export const getHeroListStage2Data = createServerFn({ method: "GET" }).handler(
   async () => readHeroListStage2Data(),
+);
+
+export const getHeroListStage3Data = createServerFn({ method: "GET" }).handler(
+  async () => readHeroListStage3Data(),
 );
