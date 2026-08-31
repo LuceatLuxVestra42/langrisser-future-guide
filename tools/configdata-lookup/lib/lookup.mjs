dd72f31c8162cce000d15be915ead62a4ab98d5f
LuceatLuxVestra42 <sapereaude1618@gmail.com>
2026-08-31 21:11:07 +0900

    feat: add ConfigData Lookup CLR2 query adapter

---
+++ tools/configdata-lookup/lib/lookup.mjs
@@ -0,0 +1,10 @@
+export {
+  findEntity,
+  loadStage5Contract,
+  lookupEntity,
+  refsEntity,
+  renderFindHuman,
+  renderJson,
+  renderLookupHuman,
+  renderRefsHuman,
+} from '../../../scripts/lib/configdata-lookup-stage5.mjs';
