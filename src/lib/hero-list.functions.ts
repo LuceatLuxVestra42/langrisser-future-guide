a1cf348eba6f31e1df6dbbad4b9743745aa30437
LuceatLuxVestra42 <sapereaude1618@gmail.com>
2026-09-15 06:23:32 +0900

    Expose HeartFetter presentation in hero detail loader

--- src/lib/hero-list.functions.ts
+++ src/lib/hero-list.functions.ts
@@ -2,6 +2,7 @@ import { createServerFn } from "@tanstack/react-start";
 
 import { applyHeroDungeonBondPresentation } from "./hero-dungeon-presentation.server";
 import { readHeroDetailRouteStage5Data } from "./hero-detail-stage5.server";
+import { readHeroHeartFetterPresentation } from "./hero-heart-fetter.server";
 import { readHeroSoldierCommand } from "./hero-soldier-command.server";
 import { resolveHeroNameLocalization } from "./hero-display-name";
 import {
@@ -248,6 +249,7 @@ export const getHeroDetailRouteStage5Data = createServerFn({ method: "GET" })
       ...routeData,
       hero: projectSharedHeroNameLocalization(routeData.hero),
       soldierCommand: readHeroSoldierCommand(data.heroId),
+      heartFetter: readHeroHeartFetterPresentation(data.heroId),
       detail: applyHeroDungeonBondPresentation(data.heroId, {
         ...routeData.detail,
         skills,
