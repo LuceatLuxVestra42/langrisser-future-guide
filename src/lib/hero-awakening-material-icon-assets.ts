ba8924b2d4d0f964f3908fdc4f2537d6e08f6994
LuceatLuxVestra42 <sapereaude1618@gmail.com>
2026-09-19 01:04:28 +0900

    Resolve awakening stage I material icons

--- src/lib/hero-awakening-material-icon-assets.ts
+++ src/lib/hero-awakening-material-icon-assets.ts
@@ -1,4 +1,11 @@
 const wikiFileNameBySourcePath = new Map<string, string>([
+  ["UI/Icon/Item_ABS/Awaken_Gem1.png", "生灵水晶.png"],
+  ["UI/Icon/Item_ABS/Awaken_Gem2.png", "苍穹水晶.png"],
+  ["UI/Icon/Item_ABS/Awaken_Gem3.png", "深渊水晶.png"],
+  ["UI/Icon/Item_ABS/Awaken_Gem4.png", "结界水晶.png"],
+  ["UI/Icon/Item_ABS/Awaken_Gem5.png", "创生水晶.png"],
+  ["UI/Icon/Item_ABS/Awaken_Gem6.png", "梦影水晶.png"],
+  ["UI/Icon/Item_ABS/Awaken_Stardust.png", "璀璨星尘.png"],
   ["UI/Icon/Item_ABS/Awaken_Heart1.png", "季风纯心.png"],
   ["UI/Icon/Item_ABS/Awaken_Heart2.png", "净涛纯心.png"],
   ["UI/Icon/Item_ABS/Awaken_Heart3.png", "大陆纯心.png"],
